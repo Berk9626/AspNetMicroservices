4affaa3047d42cb32ab43ecf5a72ff185fa36268
Berk9626 <berkberkmen12@gmail.com>
2024-01-04 18:44:07 +0300

    updated

--- src/Services/Order/Ordering.Application/Models/EmailSettings.cs
+++ src/Services/Order/Ordering.Application/Models/EmailSettings.cs
@@ -8,7 +8,7 @@ namespace Ordering.Application.Models
 {
     public class EmailSettings
     {
-        public string ApiSettings { get; set; }
+        public string ApiKey { get; set; }
         public string FromAddress { get; set; }
         public string FromName { get; set; }
     }
---
+++ src/Services/Order/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; }
+        public ILogger<EmailService> _logger { get; }
+
+        public EmailService(ILogger<EmailService> logger, IOptions<EmailSettings> emailSettings) 
+        {
+            _logger = logger;
+           _emailSettings = emailSettings.Value;
+        }
+
+        public async Task<bool> SendEmailAsync(Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+        }
+    }
+}
